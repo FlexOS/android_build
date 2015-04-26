c68d4673fb841164a3438dcae6f3f277f9c8f4f6
Flex-OS <flexos.2015@gmail.com>
2015-04-26 06:03:46 -0400

    Flexify
    Change-Id: Ibb83ccab3b40d8dcac3f9490d638c79d22f1b33d

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -34,16 +34,16 @@ except ImportError:
 
 # Config
 # set this to the default remote to use in repo
-default_rem = "omnirom"
+default_rem = "github"
 # set this to the default revision to use (branch/tag name)
-default_rev = "android-4.4"
+default_rev = "lp5.1"
 # set this to the remote that you use for projects from your team repos
 # example fetch="https://github.com/omnirom"
-default_team_rem = "omnirom"
+default_team_rem = "github"
 # this shouldn't change unless google makes changes
 local_manifest_dir = ".repo/local_manifests"
 # change this to your name on github (or equivalent hosting)
-android_team = "omnirom"
+android_team = "flex-devices"
 
 
 def check_repo_exists(git_data):
@@ -55,7 +55,7 @@ def check_repo_exists(git_data):
 # Note that this can only be done 5 times per minute
 def search_github_for_device(device):
     git_search_url = "https://api.github.com/search/repositories" \
-                     "?q=%40{}+android_device+{}+fork:true".format(android_team, device)
+                     "?q=%40{}+device+{}+fork:true".format(android_team, device)
     git_req = urllib.request.Request(git_search_url)
     # this api is a preview at the moment. accept the custom media type
     git_req.add_header('Accept', 'application/vnd.github.preview')
@@ -74,9 +74,9 @@ def get_device_url(git_data):
     device_url = ""
     for item in git_data['items']:
         temp_url = item.get('html_url')
-        if "{}/android_device".format(android_team) in temp_url:
+        if "{}/device".format(android_team) in temp_url:
             try:
-                temp_url = temp_url[temp_url.index("android_device"):]
+                temp_url = temp_url[temp_url.index("device"):]
             except ValueError:
                 pass
             else:
@@ -91,7 +91,7 @@ def get_device_url(git_data):
 
 
 def parse_device_directory(device_url,device):
-    to_strip = "android_device"
+    to_strip = "device"
     repo_name = device_url[device_url.index(to_strip) + len(to_strip):]
     repo_name = repo_name[:repo_name.index(device)]
     repo_dir = repo_name.replace("_", "/")
@@ -184,7 +184,7 @@ def write_to_manifest(manifest):
 def parse_device_from_manifest(device):
     for project in iterate_manifests():
         name = project.get('name')
-        if name.startswith("android_device_") and name.endswith(device):
+        if name.startswith("device_") and name.endswith(device):
             return project.get('path')
     return None
 
@@ -207,7 +207,7 @@ def parse_device_from_folder(device):
 
 
 def parse_dependency_file(location):
-    dep_file = "omni.dependencies"
+    dep_file = "flex.dependencies"
     dep_location = '/'.join([location, dep_file])
     if not os.path.isfile(dep_location):
         print("WARNING: %s file not found" % dep_location)
